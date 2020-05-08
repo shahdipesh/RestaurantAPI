6e8eaa06cbf3830794b19058d32a2e6fd8b7810a
shahdipesh <dipeshasd@Dipeshs-MacBook-Pro.local>
2020-05-08 04:22:03 -0500

    feat(DB):MySql_Connection

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 var express = require ('express');
-
+var pool = require('./connection');
 var app = express();
+
+var router = express.Router();
+
+app.get('/',(req,res)=>{
+    pool.query(`SELECT * FROM USER`,(err,rows)=>{
+        if (err)throw err;
+        res.send ({
+            "data":rows
+        })
+    })
+   
+})
 
 
 
